e283fe7e818c5e57a416798b5f54b66b271f76ff
Nekros <konrad.sochacki@wp.pl>
2022-05-28 18:29:29 +0200

    Wersja 1.0

--- BubbleSort_QuickSort/main.c
+++ BubbleSort_QuickSort/main.c
@@ -3,207 +3,234 @@
 #include <time.h>
 #define _CRT_SECURE_NO_WARNINGS //wylaczenie ostrzezen visuala aby nie uzywac scanf
 
-unsigned int iteracje = 0;
-unsigned int zamiany = 0;
-unsigned int porownania = 0;
-unsigned int iteracje2 = 0;
-unsigned int porownania2 = 0;
-unsigned int zamiany2 = 0;
+unsigned int iteracje = 0; //liczba iteracji sortowania babelkowego
+unsigned int zamiany = 0;// liczba zamian sortowania bablkowego
+unsigned int porownania = 0;//liczba porownan sortowania babelkowego
+unsigned int iteracje2 = 0; //liczba iteracji quicksorta
+unsigned int porownania2 = 0; //liczba porownan quicksorta
+unsigned int zamiany2 = 0; //liczba zamian quickosorta
+
 
 
 void rand_num(unsigned int* tab, unsigned int n) //Generowanie liczb pseduolosowych to tablicy
 {
-	srand(time(0));
-	int i;
+	srand(time(0)); // inicjuje ziarno generatora liczb pseudolosowych aktualnym czasem
+	int i; //iterator petli for
 	for (i = 0; i < n; i++)
 	{
-		tab[i] = 1 + rand() % 100;
+		tab[i] = 1 + rand() % 100000; //generowanie liczby z zakresu 1-100000
 
 	}
 }
 
-	wyswietlanie_tablicy(unsigned int *tab, unsigned int n) // wyswietlenie zawartosci tablicy
+wyswietlanie_tablicy(unsigned int* tab, unsigned int n) // wyswietlenie zawartosci tablicy
+{
+	int i; //iterator petli for
+	for (i = 0; i < n; i++)
 	{
-		int i;
-		for (i = 0; i < n; i++)
-		{
-			printf("[%d] = %d\n", i, tab[i]);
-			
-		}
+		printf("[%d] = %d\n", i, tab[i]);
+
 	}
+}
 
+void sortowanie_babelkowe(unsigned int* tab, unsigned int n) //sortowanie babelkowe
+{
+	int i, j; //iteratory petli for
+	int zamiana; //zmienna pomocnicza sluzaca do zamiany miejsc
 
-	void sortowanie_babelkowe(unsigned int* tab, unsigned int n) //sortowanie babelkowe
+	for (i = 0; i < n; i++)
 	{
-		int i, j, zamiana;
+		iteracje++;
 
-		for (i = 0; i < n; i++)
-		{
+		for (j = 1; j < n - i; j++)
+		{ //pêtla wewnêtrzna
+			porownania++;
 			iteracje++;
-
-			for (j = 1; j < n - i; j++)
-			{ //pêtla wewnêtrzna
-				porownania++;
-				iteracje++;
-				if (tab[j - 1] > tab[j])
-				{
-					//zamiana miejscami
-					zamiana = tab[j - 1];
-					tab[j - 1] = tab[j];
-					tab[j] = zamiana;
-					zamiany++;
-				}
+			if (tab[j - 1] > tab[j])
+			{
+				//zamiana miejscami sortowanych liczb
+				zamiana = tab[j - 1];
+				tab[j - 1] = tab[j];
+				tab[j] = zamiana;
+				zamiany++;
 			}
 		}
 	}
-	
+}
 
-	void quick_sort(int* tab, int lewy, int prawy)
+
+void quick_sort(int* tab, int lewy, int prawy) // sortowanie quick sort
+{
+	int zamiana; //zmienna pomocnicza sluzaca do zamiany miejsc
+	if (prawy <= lewy) return;
+
+	int i = lewy - 1, j = prawy + 1,
+		pivot = tab[(lewy + prawy) / 2]; //punkt odniesienia - srodkowy element tablicy
+	porownania2++; // zwiekszanie licznika liczby porownan - sortowanie quick sort
+	while (1) //petla glowna quicksorta
 	{
-		int zamiana;
-		if (prawy <= lewy) return;
-		
-			int i = lewy - 1, j = prawy + 1,
-			pivot = tab[(lewy + prawy) / 2]; //punkt odniesienia - srodkowy element tablicy
-			porownania2++;
-		while (1)
+		iteracje2++;
+
+		while (pivot > tab[++i]); //szukanie elementu wiekszego lub rownego piwot stojacego po prawej stronie wartosci pivot
+		iteracje2++;
+
+
+		while (pivot < tab[--j]);//szukanie elementu mniejszego lub rownego pivot stojacego po lewej stronie wartosci pivot
+		iteracje2++;
+
+
+		if (i <= j)//zamieñ elementy ze soba ktore stoja po niewlasciwej stronie wzgledem elementu pivot
 		{
-			iteracje2++;
-			//szukanie elementu wiekszego lub rownego piwot stojacego po prawej stronie wartosci pivot
-			while (pivot > tab[++i]);
-			iteracje2++;
+			zamiana = tab[j];
+			tab[j] = tab[i];
+			tab[i] = zamiana;
+			zamiany2++;
+		}
 
-			//szukam elementu mniejszego lub rownego pivot stojacego po lewej stronie wartosci pivot
-			while (pivot < tab[--j]);
-			iteracje2++;
+		else
 
-			//zamieñ elementy ze soba ktore stoja po niewlasciwej stronie wzgledem elementu pivot
-			if (i <= j)
-			{
-				//funkcja swap zamienia wartosciami tab[i] z tab[j]
-				
-				zamiana = tab[j];
-				tab[j] = tab[i];
-				tab[i] = zamiana;
-				zamiany2++;
-			}
-				
+			break;
+	}
+
+	if (j > lewy)
+		quick_sort(tab, lewy, j);
+	if (i < prawy)
+		quick_sort(tab, i, prawy);
+}
+
+
+int main()
+{
+	unsigned int rozmiar; //deklaracja rozmiaru tablicy
+	unsigned int* tab, * tab2; //deklaracja tablicy
+	printf("Podaj ile elementow chcesz umiescic w tablicy tablicy:");
+	scanf("%d", &rozmiar);
+	tab = (unsigned int*)malloc(sizeof(unsigned int) * rozmiar);
+	tab2 = (unsigned int*)malloc(sizeof(unsigned int) * rozmiar);
+
+	int wybor; //wybor opcji w menu
+	rand_num(tab, rozmiar);
+	tab2 = tab;
+
+	puts("Trwa inicjalizacja progamu... Prosze czekac");
+	time_t begin = clock(); //zaczecie liczenia czasu dla sortowania babelkowego
+	sortowanie_babelkowe(tab, rozmiar);
+	time_t end = clock(); //koniec liczenia czasu dla sortowania babelkowego
+
+	time_t begin2 = clock(); //zaczecie liczenia czasu dla sortowania quick sort
+	quick_sort(tab2, 0, rozmiar - 1);
+	time_t end2 = clock(); //koniec liczenia czasu dla sortowania quick sort
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; //czas wykonania sortowania babelkowego
+	double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC; //czas wykonania quickosort
+	time_spent = time_spent * 1000; // zamiana na milisekundy czasu sortowania babelkowego
+	time_spent2 = time_spent2 * 1000; // zamiana na milisekundy czasu quicksort
+	while (1) //petla glowna
+	{
+		puts("Wybierz jakiego sortowania chcesz uzyc");
+		puts("1. Sortowanie babelkowe");
+		puts("2. Quicksort");
+		puts("3. Chce porownac oba");
+		scanf("%d", &wybor);
+
+
+		switch (wybor)
+		{
+
+		case(1):
+			puts("\nWybrales sortowanie babelkowe!");
+			printf("\nCzas wykonania sortowania babelkowego wynosi: %f milisekund\n", time_spent);
+			printf("\nWykonano lacznie %u iteracji\n", iteracje);
+			printf("\nWykonano %u zamian liczb\n", zamiany);
+			printf("\nWykonano %u porownan liczb\n", porownania);
+
+			FILE* plik1; //plik zawierajacy dane wydajnosciowe z sortowania babelkowego
+			plik1 = fopen("sortowanie_babelkowe.txt", "w");
+			fprintf(plik1, "Iteracje: %d\n", iteracje);
+			fprintf(plik1, "Zamiany: %d\n", zamiany);
+			fprintf(plik1, "Porownania: %d\n", porownania);
+			fprintf(plik1, "Czas %f\n", time_spent);
+			fclose(plik1);
+			return 0;
+
+			break;
+		case(2):
+			puts("Wybrales sortowanie metoda quicksort!\n");
+			printf("\n Czas wykonania quicksort wynosi: %f milisekund\n", time_spent2);
+			printf("\nWykonano lacznie %u iteracji\n", iteracje2);
+			printf("\nWykonano %u zamian liczb\n", zamiany2);
+			printf("\nWykonano %u porownan liczb\n", porownania2);
+
+			FILE* plik2; //plik zawierajacy dane wydajnosciowe z quicksort
+			plik2 = fopen("quicksort.txt", "w");
+			fprintf(plik2, "Iteracje %d\n", iteracje);
+			fprintf(plik2, "Zamiany %d\n", zamiany);
+			fprintf(plik2, "Porownania %d\n", porownania);
+			fprintf(plik2, "Czas %f\n", time_spent2);
+			fclose(plik2);
+
+			break;
+		case(3):
+			puts("Wybrales porownanie obu metod sortowania!\n");
+			puts("SORTOWANIE BABELKOWE START");
+			if ((time_spent) <= 0)
+				printf("\nCzas wykonania sortowania babelkowego wynosi: <1 milisekundy\n");
 			else
-				
-				break;
-		}
-
-		if (j > lewy)
-			quick_sort(tab, lewy, j);
-		if (i < prawy)
-			quick_sort(tab, i, prawy);
-	}
-
-
-	int main()
-	{
-		unsigned int rozmiar; //deklaracja rozmiaru tablicy
-		unsigned int *tab,*tab2,*zerowanie; //deklaracja tablicy
-		printf("Podaj ile elementow chcesz umiescic w tablicy tablicy:");
-		scanf("%d", &rozmiar);
-		tab = (unsigned int*)malloc(sizeof(unsigned int)* rozmiar);
-		tab2 = (unsigned int*)malloc(sizeof(unsigned int) * rozmiar);
-		
-		int wybor;
-		rand_num(tab,rozmiar);
-		tab2 = tab;
-		
-		puts("Trwa inicjalizacja progamu... Prosze czekac");
-		time_t begin = clock(); //zaczecie liczenia czasu
-		sortowanie_babelkowe(tab, rozmiar);
-		time_t end = clock(); //koniec liczenia czasu
-		
-		time_t begin2 = clock();
-		quick_sort(tab2, 0, rozmiar - 1);
-		time_t end2 = clock();
-		double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-		double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
-		time_spent = time_spent * 1000;
-		time_spent2 = time_spent * 1000;
-		while (1) //petla glowna
-		{
-			puts("Wybierz jakiego sortowania chcesz uzyc");
-			puts("1. Sortowanie babelkowe");
-			puts("2. Quicksort");
-			puts("3. Chce porownac oba");
-			scanf("%d", &wybor);
-			
-
-			switch (wybor)
 			{
+				printf("\nCzas wykonania sortowania babelkowego wynosi: %f milisekund\n", (time_spent));
+			}
+			printf("\nWykonano lacznie %u iteracji\n", iteracje);
+			printf("\nWykonano %u zamian liczb\n", zamiany);
+			printf("\nWykonano %u porownan liczb\n", porownania);
+			puts("\nSORTOWANIE BABELKOWE STOP");
+			puts("\nQUICKSORT START");
+			printf("\nCzas wykonania quicksort wynosi: %f milisekund\n", time_spent2);
 
-			case(1):
-				puts("\nWybrales sortowanie babelkowe!");
-				printf("\nCzas wykonania sortowania babelkowego wynosi: %f milisekund\n", time_spent);
-				printf("\nWykonano lacznie %u iteracji\n", iteracje);
-				printf("\nWykonano %u zamian liczb\n", zamiany);
-				printf("\nWykonano %u porownan liczb\n", porownania);
-				break;
-			case(2):
-				puts("Wybrales sortowanie metoda quicksort!\n");
-				printf("\n Czas wykonania quicksort wynosi: %f milisekund\n", time_spent2);
-				printf("\nWykonano lacznie %u iteracji\n", iteracje2);
-				printf("\nWykonano %u zamian liczb\n", zamiany2);
-				printf("\nWykonano %u porownan liczb\n", porownania2);
 
-				break;
-			case(3):
-				puts("Wybrales porownanie obu metod sortowania!\n");
-				//wyswietlanie_tablicy(tab2, rozmiar);
-				//puts("SPACJA");
-				//wyswietlanie_tablicy(tab, rozmiar);
-				puts("SORTOWANIE BABELKOWE START");
-				if ((time_spent) <= 0)
-					printf("\nCzas wykonania sortowania babelkowego wynosi: <1 milisekundy\n");
-				else
-				{
-					printf("\nCzas wykonania sortowania babelkowego wynosi: %f milisekund\n", (time_spent));
-				}
-				printf("\nWykonano lacznie %u iteracji\n", iteracje);
-				printf("\nWykonano %u zamian liczb\n", zamiany);
-				printf("\nWykonano %u porownan liczb\n", porownania);
-				puts("\nSORTOWANIE BABELKOWE STOP");
-				puts("\nQUICKSORT START");
-				printf("\nCzas wykonania quicksort wynosi: %f milisekund\n", time_spent2);
-				
-			
-				printf("\nWykonano lacznie %u iteracji\n", iteracje2);
-				printf("\nWykonano %u zamian liczb\n", zamiany2);
-				printf("\nWykonano %u porownan liczb\n", porownania2);
-				puts("\nQUICKSORT STOP\n");
+			printf("\nWykonano lacznie %u iteracji\n", iteracje2);
+			printf("\nWykonano %u zamian liczb\n", zamiany2);
+			printf("\nWykonano %u porownan liczb\n", porownania2);
+			puts("\nQUICKSORT STOP\n");
 
-				//wyswietlanie_tablicy(tab2, rozmiar); //Wlaczyc zeby sprawdzilo czy sortuje
+			FILE* plik3; //plik zawierajacy dane wydajnosciowe z obu sortowan
+			plik3 = fopen("obie_metody.txt", "a");
+			fprintf(plik3, "\n\nRozmiar tablicy %d\n\n", rozmiar);
+			fprintf(plik3, "Sortowanie babelkowe : \n");
+			fprintf(plik3, "Iteracje %d\n", iteracje);
+			fprintf(plik3, "Zamiany %d\n", zamiany);
+			fprintf(plik3, "Porownania %d\n", porownania);
+			fprintf(plik3, "Czas %f\n", time_spent);
+			fprintf(plik3, " \n");
+			fprintf(plik3, "Quick sort : \n");
+			fprintf(plik3, "Iteracje %d\n", iteracje2);
+			fprintf(plik3, "Zamiany %d\n", zamiany2);
+			fprintf(plik3, "Porownania %d\n", porownania2);
+			fprintf(plik3, "Czas %f\n", time_spent2);
+			fclose(plik3);
 
-				if (time_spent2 < time_spent)
-				{
-					printf("\nW tym wypadku quicksort byl szybszy o %f sekund\n ", time_spent - time_spent2);
-				}
-
-				else if (time_spent < time_spent2)
-				{
-					printf("\nW tym wypadku quicksort byl szybszy o %f milisekund\n ", time_spent2 - time_spent);
-				}
-
-				else
-					printf(" \n W tym wypadku czasy quicksort i bubble sort byly mniejsze niz 1 milisekunda\n\n");
-				
-
-				break;
-			default:
-				printf("\nMusisz wybrac jedna z 3 opcji: ");
-				break;
+			if (time_spent2 < time_spent)
+			{
+				printf("\nW tym wypadku quicksort byl szybszy o %f milisekund\n ", time_spent - time_spent2);
 			}
 
-		} 
-		
-		//wyswietlanie_tablicy(tab, rozmiar); // tymczasowo wylaczone
-		
+			else if (time_spent < time_spent2)
+			{
+				printf("\nW tym wypadku quicksort byl szybszy o %f milisekund\n ", time_spent2 - time_spent);
+			}
 
-		
-		return 0;
+			else
+				printf(" \n W tym wypadku czasy quicksort i bubble sort byly mniejsze niz 1 milisekunda\n\n");
+
+
+			break;
+		default:
+			printf("\nMusisz wybrac jedna z 3 opcji: ");
+			break;
+		}
+
 	}
+	/*
+	* \author Konrad Sochacki
+	* \author Piotr Skuta
+	*/
+	return 0;
+}
